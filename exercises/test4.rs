781b50cb99459f0ffba27e15c3b7ffbc6e30c9a2
horochx <horochx@gmail.com>
2020-03-30 23:03:25 +0800

    pass exercise: test4

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,19 +5,25 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn test_my_macro_world() {
         assert_eq!(my_macro!("world!"), "Hello world!");
     }
-
+    
     #[test]
     fn test_my_macro_goodbye() {
         assert_eq!(my_macro!("goodbye!"), "Hello goodbye!");
+    }
+}
+
+#[macro_export]
+macro_rules! my_macro {
+    ($val:expr) => {
+        "Hello ".to_owned() + $val
     }
 }
